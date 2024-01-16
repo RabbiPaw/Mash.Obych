7bc2a493d3e0d9155fcc509ab3359818bb0e13c5
Святослав Салабун <124811370+RabbiPaw@users.noreply.github.com>
2024-01-16 13:25:30 +0600

    Update Predictionsa_with_self_data.py

--- notebooks/RGR/pages/Predictionsa_with_self_data.py
+++ notebooks/RGR/pages/Predictionsa_with_self_data.py
@@ -65,7 +65,7 @@ model_name = st.selectbox(
 def null(x): return x.empty if type(x) is pd.DataFrame else not x 
 
 if model_name and not null(df):
-  model = pickle.load(open(f'models/{model_name}.pickle', 'rb'))
+  model = pickle.load(open(f'models/{model_name}.pickle'))
 
   transformer = pickle.load(open('models/PolynomialFeatures.pickle', 'rb'))
 
